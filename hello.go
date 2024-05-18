8c846d6661a0a0066a65eef56f1c28e7229549db
morfal <markfirn@gmail.com>
2024-05-18 19:54:47 +0200

    ex-mailer: Add exchange rate API, subscribe endpoint

--- hello.go
+++ hello.go
@@ -1,64 +1,101 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-var db = make(map[string]string)
+var db *sqlx.DB
+
+type RateMeasurement struct {
+	Timestamp time.Time `db:"timestamp" json:"timestamp"`
+	Value     float64   `db:"value" json:"value"`
+}
+
+type Subscription struct {
+	Timestamp time.Time `db:"timestamp" json:"timestamp"`
+	Email     string    `db:"email" json:"email"`
+}
+
+type ExchangeRateResp struct {
+	RatesMap map[string]float64 `json:"ratesmap"`
+	Date     time.Time          `json:"date"`
+}
+
+func getRate() (RateMeasurement, error) {
+	rate, err := getLastRate()
+	if err != nil || time.Since(rate.Timestamp) > time.Hour {
+		err := updateRate()
+		if err != nil {
+			return RateMeasurement{}, err
+		}
+		rate, err = getRate()
+		if err != nil {
+			return RateMeasurement{}, err
+		}
+	}
+	return rate, nil
+}
+
+func trySubscribe(email string) error {
+	// if database has email -> return error
+	// otherwise add a new email to the database
+}
+
+func getLastRate() (RateMeasurement, error) {
+	var rate RateMeasurement
+	err := db.Get(&rate, "SELECT timestamp, value FROM usd_uah_rates ORDER BY timestamp DESC LIMIT 1")
+	if err != nil {
+		return RateMeasurement{}, err
+	}
+	return rate, nil
+}
+
+func updateRate() error {
+	// go to USD/UAH exchange API and add a new rate measurement to the database
+	resp, err := http.Get("https://api.exchangerate-api.com/v4/latest/USD")
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+}
+
+func setupDatabase() *sqlx.DB {
+	db, err := sqlx.Connect("sqlite3", "exchange.db")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return db
+}
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 
-	// Ping test
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
 
-	// // Get user value
-	// r.GET("/user/:name", func(c *gin.Context) {
-	// 	user := c.Params.ByName("name")
-	// 	value, ok := db[user]
-	// 	if ok {
-	// 		c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-	// 	} else {
-	// 		c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-	// 	}
-	// })
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	// authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-	// 	"foo":  "bar", // user:foo password:bar
-	// 	"manu": "123", // user:manu password:123
-	// }))
-
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	r.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
+	r.GET("/rate", func(c *gin.Context) {
+		var rate, err = getRate()
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("bad request, error: %s", err.Error()))
 		}
+		c.String(http.StatusOK, fmt.Sprintf("%.2f", rate.Value))
+	})
 
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	r.POST("/subscribe/:email", func(c *gin.Context) {
+		err := trySubscribe(c.Param("email"))
+		if err != nil {
+			c.JSON(http.StatusConflict, gin.H{"status": "subscription conflict", "error": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"status": "subscribed"})
 		}
 	})
 
